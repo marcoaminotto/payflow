9eb8f35e253fbbe89ee78fb41b2ad520b73f3879
Marco Echevestre <marco.echevestre@sofomo.com>
2022-02-04 18:16:04 +0100

    Create login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/login/login_page.dart';
 import 'package:payflow/modules/splash/splash_page.dart';
 import 'package:payflow/shared/themes/appcolors.dart';
 
@@ -17,7 +18,7 @@ class AppWidget extends StatelessWidget {
       theme: ThemeData(
         primaryColor: AppColors.primary,
       ),
-      home: SplashPage(),
+      home: LoginPage(),
     );
   }
 }
